9b126b71920131fb8ec98bf2b631c0a16a4f07ba
Anikait <50035624+anikait1@users.noreply.github.com>
2020-12-14 10:24:29 +0530

    Updated the namespace reference

--- BackEnd/Controllers/SpeakersController.cs
+++ BackEnd/Controllers/SpeakersController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BackEnd.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using BackEnd.Models;
+
 
 namespace BackEnd.Controllers
 {
